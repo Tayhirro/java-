ed8ba2852a59d6b775a73837226c13410792c7e5
ly <1668113737@qq.com>
2024-05-08 03:50:37 +0800

    修复了关闭地图会关闭所有窗口的bug 给路线规划加个小窗口

--- src/RoutePlanningSystem.java
+++ src/RoutePlanningSystem.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class RoutePlanningSystem {
 
     // 点的类型，路口，建筑，地铁站，公交站，景点等
@@ -10,6 +14,8 @@ public class RoutePlanningSystem {
     }
     // 点类
     class MyPoint {
+
+        String name;    //点的名字
         int x;          //点的坐标
         int y;           //点的坐标
         PointType type; // 点的类型
@@ -19,7 +25,8 @@ public class RoutePlanningSystem {
             this.y = y;
         }
 
-        public MyPoint(int x, int y, PointType type) {
+        public MyPoint(String name, int x, int y, PointType type) {
+            this.name = name;
             this.x = x;
             this.y = y;
             this.type = type;
@@ -83,11 +90,26 @@ public class RoutePlanningSystem {
             points[pointNum] = new MyPoint(x, y);
             pointNum++;
         }
+        void addPoint(String name, int x, int y, PointType type) {
+            points[pointNum] = new MyPoint(name, x, y, type);
+            pointNum++;
+        }
         void addEdge(int from, int to, int weight, int length, int crowding, EdgeType type) {
             edges[edgeNum] = new MyEdge(from, to, heads[from] , weight, length, crowding, type);
             heads[from] = edgeNum;
             edgeNum++;
         }
+
+        // 获取点的编号
+        int getPointId(String name){
+            for(int i = 0;i<pointNum;i++){
+                if(points[i].name.equals(name)){
+                    return i;
+                }
+            }
+            return -1;
+        }
+
         // 获取边的编号
         int getEdgeId (int from,int to){
             //遍历from的边
@@ -98,6 +120,40 @@ public class RoutePlanningSystem {
             }
             return -1;
         }
+        void loadFromFile(String pointsFilePath, String edgesFilePath) {
+            try {
+                // Load points
+                Scanner pointsScanner = new Scanner(new File(pointsFilePath));
+                while (pointsScanner.hasNextLine()) {
+                    String line = pointsScanner.nextLine();
+                    String[] parts = line.split(",");
+                    String name = parts[0];
+                    int x = Integer.parseInt(parts[1]);
+                    int y = Integer.parseInt(parts[2]);
+                    PointType type = PointType.valueOf(parts[3]);
+                    addPoint(name, x, y, type);
+                }
+                pointsScanner.close();
+
+                // Load edges
+                Scanner edgesScanner = new Scanner(new File(edgesFilePath));
+                while (edgesScanner.hasNextLine()) {
+                    String line = edgesScanner.nextLine();
+                    String[] parts = line.split(",");
+                    int from = Integer.parseInt(parts[0]);
+                    int to = Integer.parseInt(parts[1]);
+                    int weight = Integer.parseInt(parts[2]);
+                    int length = Integer.parseInt(parts[3]);
+                    int crowding = Integer.parseInt(parts[4]);
+                    EdgeType type = EdgeType.valueOf(parts[5]);
+                    addEdge(from, to, weight, length, crowding, type);
+                }
+                edgesScanner.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found: " + e.getMessage());
+            }
+        }
+
 
     }
 
