a1ea48bad59d9c8df0a1f341c5f4135782a8647d
Cheeq <jqli_2022@163.com>
2024-05-07 21:36:05 +0800

    基本

--- src/CenterPlace.java
+++ src/CenterPlace.java
@@ -1,2 +1,33 @@
+import java.util.List;
+
 public class CenterPlace {
+    private String name;
+    private double popularity;//热度
+    private double rating;//评价
+    private List<String> features; // Features related to user interests
+
+    public CenterPlace(String name, double popularity, double rating, List<String> features) {
+        this.name = name;
+        this.popularity = popularity;
+        this.rating = rating;
+        this.features = features;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public double getPopularity() {
+        return popularity;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public List<String> getFeatures() {
+        return features;
+    }
+
 }
