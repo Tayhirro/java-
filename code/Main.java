ef9c8fbe1b8cff443c14a919e85c88917acdb520
ly <1668113737@qq.com>
2024-06-01 04:10:21 +0800

    修复了地址bug，改了下文档

--- code/Main.java
+++ code/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        UserManagement userManagement = new UserManagement("//data//userData.ser");// 创建用户管理对象
+        UserManagement userManagement = new UserManagement("data\\userData.ser");// 创建用户管理对象
         new LoginApp(userManagement);// 创建登录界面对象,并传入用户管理对象
     }
 
